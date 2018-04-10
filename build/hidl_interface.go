AdrianDC/aosp_development_sony8960_p

Build HIDL Java targets with SystemApi.
They should all link only against SystemApi.

Bug: 72480743
Test: build java targets
Change-Id: I7fd0061a410b08a0e8370742d512aeb6807d2600

build/hidl_interface.go
@@ -288,10 +288,11 @@ func hidlInterfaceMutator(mctx android.LoadHookContext, i *hidlInterface) {
 		mctx.CreateModule(android.ModuleFactoryAdaptor(java.LibraryFactory(true)), &javaProperties{
 			Name:              proptools.StringPtr(name.javaName()),
 			Owner:             i.properties.Owner,
+			Sdk_version:       proptools.StringPtr("system_current"),
 			Defaults:          []string{"hidl-java-module-defaults"},
 			No_framework_libs: proptools.BoolPtr(true),
 			Srcs:              []string{":" + name.javaSourcesName()},
-			Libs:              append(javaDependencies, "hwbinder"),
+			Libs:              javaDependencies,
 		})
 	}
 

LineageOS/android_system_tools_hidl

Merged-In: I7fd0061a410b08a0e8370742d512aeb6807d2600
(cherry picked from commit c861dcfec9afd75123889a7efa6fcfd4b431126a)

build/hidl_interface.go
@@ -289,10 +289,11 @@ func hidlInterfaceMutator(mctx android.LoadHookContext, i *hidlInterface) {
 		mctx.CreateModule(android.ModuleFactoryAdaptor(java.LibraryFactory(true)), &javaProperties{
 			Name:              proptools.StringPtr(name.javaName()),
 			Owner:             i.properties.Owner,
+			Sdk_version:       proptools.StringPtr("system_current"),
 			Defaults:          []string{"hidl-java-module-defaults"},
 			No_framework_libs: proptools.BoolPtr(true),
 			Srcs:              []string{":" + name.javaSourcesName()},
-			Static_libs:       append(javaDependencies, "hwbinder"),
+			Static_libs:       javaDependencies,
 		})
 	}
 