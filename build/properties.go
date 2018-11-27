e5faf7ec83b4dddaee9df4886d198b246f58b82d
Justin Yun <justinyun@google.com>
2018-11-27 12:22:21 +0900

    Add product_specific property in hidl_interface
    Use "product_specific: true" to install core variant of the hidl
    interface on product partition.
    
    Bug: 119575360
    Test: build a hidl_interface with "product_specific: true".
    Change-Id: Ideeaf41ae8232115075dd6aa8cff4392eee4f0c5

--- build/properties.go
+++ build/properties.go
@@ -28,6 +28,7 @@ type ccProperties struct {
 	Name                      *string
 	Owner                     *string
 	Defaults                  []string
+	Product_specific          *bool
 	Vendor_available          *bool
 	Recovery_available        *bool
 	Generated_sources         []string
