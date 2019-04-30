48cc604548c6dcd8efffe3cd10cd7537312efd22
Steven Moreland <smoreland@google.com>
2019-04-30 11:28:56 -0700

    BpHw* transact uses callback.
    For Java passthrough case and for non-binder protocols.
    
    Bug: 129150021
    Test: ./test/run_all_device_tests.sh
    Test: libhwbinder_benchmark (within variance)
    Change-Id: I8830796bc48d822e494b197422e4ab0f26f45524

--- Type.cpp
+++ Type.cpp
@@ -525,7 +525,7 @@ void Type::emitJavaFieldReaderWriter(
     CHECK(!"Should not be here") << typeName();
 }
 
-void Type::handleError(Formatter &out, ErrorMode mode) const {
+void Type::handleError(Formatter &out, ErrorMode mode) {
     switch (mode) {
         case ErrorMode_Ignore:
         {
@@ -549,6 +549,16 @@ void Type::handleError(Formatter &out, ErrorMode mode) const {
         {
             out << "if (_hidl_err != ::android::OK) { return _hidl_err; }\n\n";
             break;
+        }
+
+        case ErrorMode_ReturnNothing:
+        {
+            out << "if (_hidl_err != ::android::OK) { return; }\n\n";
+            break;
+        }
+        default:
+        {
+            LOG(FATAL) << "Should not be here";
         }
     }
 }
