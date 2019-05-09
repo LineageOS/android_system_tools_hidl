4b6f4394b188167c91465bc2f65a4d89c4ff85d4
Neel Mehta <mneel@google.com>
2019-05-09 16:03:47 -0700

    Add -Ljava-impl for HIDL
    This generates a default Java implementation of the interfaces.
    
    It generates
    - for methods that return
    ```
    @Override
    public boolean someBoolMethod(boolean x)
            throws android.os.RemoteException {
        // TODO: Implement
        boolean y = false;
        return y;
    }
    ```
    - for methods with callbacks
    ```
    @Override
    public void takeAMask(byte bf, byte first, android.hardware.tests.baz.V1_0.IBase.MyMask second, byte third, takeAMaskCallback _hidl_cb)
            throws android.os.RemoteException {
        // TODO: Implement
        byte out = 0;
        byte f = 0;
        byte s = 0;
        byte t = 0;
        _hidl_cb.onValues(out, f, s, t);
    }
    ```
    
    Fix:132265341
    Test: m hidl_java_impl_test
    Change-Id: I1f03f4aac45fdb2b1afc87a4eb9c2f30461c846e

---
+++ generateJavaImpl.cpp
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "AST.h"
+#include "Interface.h"
+#include "Method.h"
+
+namespace android {
+void AST::generateJavaImpl(Formatter& out) const {
+    if (!AST::isInterface()) {
+        // types.hal does not get a stub header.
+        return;
+    }
+
+    const Interface* iface = mRootScope.getInterface();
+    const std::string baseName = iface->getBaseName();
+
+    out << "// FIXME: your file license if you have one\n\n";
+    out << "// FIXME: add package information\n\n";
+
+    out << "import " << mPackage.javaPackage() << "." << iface->localName() << ";\n\n";
+
+    out.setNamespace(mPackage.javaPackage() + ".");
+
+    out << "class " << baseName << " extends " << iface->localName() << ".Stub"
+        << " {\n";
+
+    out.indent([&] {
+        const Interface* prevInterface = nullptr;
+        for (const auto& tuple : iface->allMethodsFromRoot()) {
+            const Method* method = tuple.method();
+
+            if (method->isHidlReserved()) {
+                continue;
+            }
+
+            const Interface* superInterface = tuple.interface();
+            if (prevInterface != superInterface) {
+                out << "// Methods from " << superInterface->fullJavaName() << " follow.\n";
+                prevInterface = superInterface;
+            }
+
+            const bool returnsValue = !method->results().empty();
+            const bool needsCallback = method->results().size() > 1;
+
+            out << "@Override\npublic ";
+            if (returnsValue && !needsCallback) {
+                out << method->results()[0]->type().getJavaType();
+            } else {
+                out << "void";
+            }
+
+            out << " " << method->name() << "(";
+            method->emitJavaArgSignature(out);
+
+            if (needsCallback) {
+                if (!method->args().empty()) {
+                    out << ", ";
+                }
+
+                out << method->name() << "Callback _hidl_cb";
+            }
+
+            out << ")\n";
+            out.indent([&] {
+                out.indent();
+                out << "throws android.os.RemoteException {\n";
+                out.unindent();
+
+                out << "// TODO: Implement\n";
+
+                // Return the appropriate value
+                if (returnsValue) {
+                    for (const auto& arg : method->results()) {
+                        arg->type().emitJavaFieldInitializer(out, arg->name());
+                    }
+
+                    if (needsCallback) {
+                        out << "_hidl_cb.onValues(";
+
+                        out.join(method->results().begin(), method->results().end(), ", ",
+                                 [&](auto& arg) { out << arg->name(); });
+
+                        out << ");\n";
+                    } else {
+                        // only 1 value is returned
+                        out << "return " << method->results().at(0)->name() << ";\n";
+                    }
+                }
+            });
+
+            out << "}\n\n";
+        }
+    });
+
+    out << "}\n";
+}
+}  // namespace android
